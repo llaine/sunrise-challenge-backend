d94e3a6f9145aabdd2ae99befa6fcdcc5dee98e6
llaine <darksioul6@gmail.com>
2015-03-14 17:51:31 +0100

    bonusLogFormating(feat): New version of the format library. Adding the unit test. Adding JSHINT packages

--- src/logBonus.js
+++ src/logBonus.js
@@ -1,11 +1,33 @@
 /**
- * sunrise-backend
+ * sunrise-backend BONUS Log Formating library
  *
+ *
+ * Take an array of string and must input one string formated.
+ *
+ * Output :
+ *
+ HTTP Request
+        URL http://example.com
+     Headers
+        Authorization: Bearer oauth-token
+        Accept: application/json
+     Timeout 25s
+Response
+    time: 500ms
+    length: 85KB
+Parsing event [id:1]
+    No title
+    3 attendees
+Parsing event [id:2]
+    Title: "Lunch"
+    0 attendees
+ *
+ *
+ *
+ * To the the prototype of anwsers
  * @user llaine
  * @date 13/03/15
  */
-// Code goes here
-console.clear();
 
 var logs = [
     'HTTP Request',
@@ -22,49 +44,77 @@ var logs = [
 ];
 
 
-/*
- HTTP Request
- URL http://example.com
- Headers
- Authorization: Bearer oauth-token
- Accept: application/json
- Timeout 25s
- Response
- time: 500ms
- length: 85KB
- Parsing event [id:1]
- No title
- 3 attendees
- Parsing event [id:2]
- Title: "Lunch"
- 0 attendees
- */
-
 String.prototype.repeat = function(){
     return new Array(arguments[0] + 1).join(this);
 };
 
-logs.map(function(value, index, array) {
-    var currentValueArray = value.split(' ');
+/**
+ * console.log(log) will return the desired output.
+ *
+ * @return String
+ */
+var log = logs.map(function(value, index, array) {
+    /**
+     * Main function for formatting each lines.
+     * Check words between the current and the previous line,
+     * format the result.
+     *
+     * @param currentWord : String
+     * @param indexInCurrentRow : Number
+     * @param arrayOfWord : Array
+     * @returns {*}
+     */
+    function formatRow(currentWord, indexInCurrentRow, arrayOfWord){
+        /* variable to store the formated row */
+        var formatedRow;
+        /**
+         * Adds a line feed and respects the number of space at the beginning of the next line.
+         *
+         * @param nb
+         * @return String
+         **/
+        function jumpAndTab(nb){
+            return formatedRow + "\n" + " ".repeat(nb);
+        }
 
-    /* Previous values */
-    var previousValue = array[index-1];
-    var previousValArray = previousValue ? previousValue.split(' ') : null;
-
-    /* e: value, i : index, a : array */
-    words = currentValueArray.map(function(e, i, a){
-        console.log(a);
-        var valueToBeReturned;
-        var tab = " ".repeat(i);
-        /* The same word appear between */
-        if(previousValue){
-            console.log(previousValue);
-            valueToBeReturned = previousValArray[i] !== e ? e : tab;
+        /* Avoid Range Exception */
+        if(previousRow){
+            /* Check if the previous value is not equals to the current value */
+            formatedRow = arrayPreviousRow[indexInCurrentRow] !== currentWord ? currentWord : " ".repeat(indexInCurrentRow);
+            /*
+             For the event log, we want to keep the "Parsing event" string
+             when the id of the event changes.
+             */
+            if(currentWord === "event" && arrayPreviousRow[indexInCurrentRow+1] !== arrayOfWord[indexInCurrentRow+1]){
+                if(formatedRow !== "event"){
+                    /* formatedRow = Parsing + " " + event */
+                    formatedRow = arrayOfWord[indexInCurrentRow - 1] + " " + arrayOfWord[indexInCurrentRow];
+                }
+            }
         }else{
             /* First row, returning the content */
-            valueToBeReturned = e;
+            formatedRow = currentWord;
         }
-        return valueToBeReturned;
-    }).join(" ");
-    //console.log(words);
-});
+
+        /* Jump Line if we found Headers or Response, or match an event id */
+        if(formatedRow === "Headers") return jumpAndTab(5);
+        if(formatedRow === "Response") return jumpAndTab(0);
+        /* Match the [id:1],  [id:2] */
+        if(formatedRow.match(/\[[A-z].*[0-9]+\]/)){
+            return jumpAndTab(5);
+        }
+        return formatedRow;
+    }
+
+
+    var row = value.split(' ');
+    /* Previous row compared to the current index. */
+    var previousRow = array[index-1];
+    /* Splitting the previous STRING row in an Array. Ex : "Http Request" -> ["HTTP", "Request"] */
+    var arrayPreviousRow = previousRow ? previousRow.split(' ') : null;
+    /* Format the arrayRow and convert each row separate by blank space in String. */
+    return row.map(formatRow).join(" ");
+
+}).join('\n');
+
+module.exports = log;
---
+++ test/logBonus.test.js
@@ -0,0 +1,44 @@
+/**
+ * sunrise-backend
+ *
+ * @user llaine
+ * @date 14/03/15
+ */
+
+
+/* Assert tools */
+var assert = require('assert');
+
+var logBonus = require("../src/logBonus");
+
+
+var logs = [
+    'HTTP Request',
+    'HTTP Request URL http://example.com',
+    'HTTP Request Headers Authorization: Bearer oauth-token',
+    'HTTP Request Headers Accept: application/json',
+    'HTTP Request Timeout 25s',
+    'Response time: 500ms',
+    'Response length: 85KB',
+    'Parsing event [id:1] No title',
+    'Parsing event [id:1] 3 attendees',
+    'Parsing event [id:2] Title: "Lunch"',
+    'Parsing event [id:2] 0 attendees'
+];
+
+var output = "HTTP Request\n   URL http://example.com\n   Headers\n      Authorization: Bearer oauth-token\n      Accept: application/json\n   Timeout 25s\nResponse\n time: 500ms\n length: 85KB\nParsing event [id:1]\n      No title\n      3 attendees\n Parsing event [id:2]\n      Title: \"Lunch\"\n      0 attendees";
+
+
+
+
+describe("Log Bonus", function () {
+    describe("#format()", function () {
+        it('Should return the desired ouput', function () {
+            console.warn("OUPUT :");
+            console.log(logBonus);
+            console.warn("END OUPUT");
+
+            assert.deepEqual(logBonus, output);
+        });
+    });
+});
